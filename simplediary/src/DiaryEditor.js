afcf39f9b678132ed1a0b6ac8ec6fd8de5df856e
Nowee <nowee018@naver.com>
2023-02-17 15:32:33 +0900

    focus 주는 기능

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -2,9 +2,15 @@ import {useState} from "react";
 
 
 const DiaryEditor = () =>{
+
+    const authorInput = useRef();
+    const contentInput = useRef();
+
     const [state, setState] = useState({
         author:"",
-        content:""
+        content:"",
+        emotion:1,
+
     })
 
     const handleChangeState = (e) => {
@@ -17,21 +23,54 @@ const DiaryEditor = () =>{
 
         })
     }
+    const handleSubmit = () => {
+        if(state.author.length < 1){
+            authorInput.current.focus(); // focus를 주는 기능 
+            return ;
+        }
+        if(state.content.length < 5){
+            contentInput.current.focus();
+            return;
+        }
+        
+        // {author : 곽수찬, content : 오늘의 일기2, emotion : 2 }
+        alert('저장 성공')
+    }
 
-    return <div classname="DiaryEditor">
+    return (
+    <div className="DiaryEditor">
 
         <h2>오늘의 일기</h2>
-        <input name ="author"value= {state.author} 
-            onChange={handleChangeState}/>
         <div>
-            <textarea name = "content" value={state.content} 
+        <input
+            ref = {authorInput} 
+            name ="author"
+            value= {state.author} 
+            onChange={handleChangeState}/>
+        </div>
+        <div>
+            <textarea
+            ref = {contentInput}  
+            name = "content" 
+            value={state.content} 
             onChange={handleChangeState}/>
         </div>
 
         <div>
-            <textarea name = "content" value={state.content} 
-            onChange={handleChangeState}/>
+           <select name='emotion' 
+           value={state.emotion} 
+           onChange={handleChangeState}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+           </select>
         </div>
-    </div>;
+        <div>
+            <button onClick={handleSubmit}>일기 저장하기</button>
+        </div>
+    </div>
+    );
 };
 export default DiaryEditor;
