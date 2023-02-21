1a162e737946a5c216f3b34569690944fbe927a7
Nowee <nowee018@naver.com>
2023-02-21 11:11:03 +0900

    수정하기 버튼 추가

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,62 +1,44 @@
-import React,{useRef, useState} from "react";
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
-
-// const dummyList = [
-//   {
-//     id:1,
-//     author:"곽수찬",
-//     content:"하이 1",
-//     emotion:5,
-//     created_date : new Date().getTime()
-//   },
-//   {
-//     id:2,
-//     author:"아무개",
-//     content:"하이 2",
-//     emotion:2,
-//     created_date : new Date().getTime()
-//   },
-//   {
-//     id:3,
-//     author:"곽수찬",
-//     content:"하이 3",
-//     emotion:3,
-//     created_date : new Date().getTime()
-//   },
-// ]
-function App() {
+const App = () => {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
   const onCreate = (author, content, emotion) => {
-    const created_data = new Date().getTime();
+    const created_date = new Date().getTime();
     const newItem = {
       author,
       content,
       emotion,
-      created_data,
-      id : dataId.current,
-    }
-
+      created_date,
+      id: dataId.current
+    };
     dataId.current += 1;
-    setData([...data, newItem]);
+    setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId)=>{
-    console.log(`${targetId}가 삭제되었습니다.`);
-    const newDiaryList = data.filter((it)=> it.id != targetId);
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
-  }
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
-     <DiaryEditor onCreate = {onCreate}/>
-     <DiaryList onDelete = {onDelete} diaryList={data}/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
-
+};
 export default App;
--- simplediary/src/DiaryList.js
+++ simplediary/src/DiaryList.js
@@ -1,14 +1,14 @@
 import DiaryItem from './DiaryItem';
 
 
-const DiaryList = ({onDelete, diaryList}) =>{
+const DiaryList = ({onEdit, onRemove, diaryList}) =>{
     console.log(diaryList);
     return <div className ="DiaryList">
         <h2>일기 리스트</h2>
         <h4>{diaryList.length}개의 일기가 있습니다.</h4>
         <div>
             {diaryList.map((it,idx)=>(
-            <DiaryItem key={it.id} {...it} onDelete = {onDelete}/>            
+            <DiaryItem key={it.id} {...it} onDelete = {onRemove}/>            
             ))}
         </div>
     </div>
