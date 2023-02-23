2a8eb46da0baca8a0ce1a50dc349335a98cce6d6
Nowee <nowee018@naver.com>
2023-02-23 09:34:29 +0900

    수정하기 추가

--- emotion-diary/src/components/DiaryItem.js
+++ emotion-diary/src/components/DiaryItem.js
@@ -1,7 +1,20 @@
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+
 const DiaryItem = ({id, emotion, content, date}) =>{
+    const navigate = useNavigate();
+    const strDate = new Date(parseInt(date)).toLocaleDateString();
+    const goEdit = ()=>{
+        navigate(`/edit/${id}`)
+    }
+    const goDetail = () =>{
+        navigate (`/diary/${id}`)
+    }
+
     return <div className="DiaryItem">
 
-    <div 
+    <div
+    onClick = {goDetail} 
     className={[
         "emotion_img_wrapper",
         `emotion_img_wrapper_${emotion}`,
@@ -9,11 +22,15 @@ const DiaryItem = ({id, emotion, content, date}) =>{
     >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}/>
     </div>
-    <div>
-
+    <div onClick = {goDetail} className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0,25)}</div>
     </div>
-    <div>
 
+
+
+    <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
 
     </div>
 
